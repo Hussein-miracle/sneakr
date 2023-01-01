ea709fb4ea03db00c88784ba528532cea6a8e2a8
Abdullahi <miraacle64@gmail.com>
2023-01-01 03:31:46 +0100

    feat: initial commit exited to build sneakr

---
+++ App.js
@@ -0,0 +1,50 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'syne': require('./assets/fonts/Syne-Regular-400.ttf'),
+    'tenor-sans': require('./assets/fonts/Tenor-Sans-400.ttf'),
+  });
+
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded){
+    return null;
+  }
+
+
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
